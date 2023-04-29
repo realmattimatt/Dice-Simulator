6879dc8e6bea202bf89573c2547fe122a9eaf899
Matt Matti <128356316+realmattimatt@users.noreply.github.com>
2023-04-29 18:17:09 -0500

    First Commit 1st Draft

---
+++ Dice Simulator copy.py
@@ -0,0 +1,67 @@
+import random
+# Symbols to make dice
+# Alt 196 ─
+# Alt 217 ┘
+# Alt 218 ┌
+# Alt 191 ┐
+# Alt 192 └
+# Alt 179 │
+# Alt 9   ○ 
+
+def roll_dice():
+    dice_drawing = {
+        1: (
+            "┌─────────┐",
+            "│    1    │",
+            "│    ○    │",
+            "│         │",
+            "└─────────┘",
+        ),
+        2: (
+            "┌─────────┐",
+            "│  ○      │",
+            "│    2    │",
+            "│      ○  │",
+            "└─────────┘",
+        ),
+        3: (
+            "┌─────────┐",
+            "│  ○ 3    │",
+            "│    ○    │",
+            "│      ○  │",
+            "└─────────┘",
+        ),
+        4: (
+            "┌─────────┐",
+            "│  ○   ○  │",
+            "│    4    │",
+            "│  ○   ○  │",
+            "└─────────┘",
+        ),
+        5: (
+            "┌─────────┐",
+            "│  ○ 5 ○  │",
+            "│    ○    │",
+            "│  ○   ○  │",
+            "└─────────┘",
+        ),    
+        6: (
+            "┌─────────┐",
+            "│  ○   ○  │",
+            "│  ○ 6 ○  │",
+            "│  ○   ○  │",
+            "└─────────┘",
+        )
+    }
+    roll = input("Roll the dice? (Yes or No): ")
+    while roll.lower() == "Yes".lower():
+        dice1 = random.randint(1,6)
+        dice2 = random.randint(1,6)
+        
+        print(f"dice rolled: {dice1} and {dice2}")
+        print("\n".join(dice_drawing[dice1]))
+        print("\n".join(dice_drawing[dice2]))
+        roll = input("Roll again? (Yes / NO): ")
+
+        
+roll_dice()
